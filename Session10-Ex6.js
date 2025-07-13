a17d4202009a9d79ff3741a22be57ecc03da1796
vuongcq <quyvuong991@gmail.com>
2025-07-13 15:58:34 +0900

    Add files via upload
    Upload Session10-ex6

---
+++ Session10-Ex6.js
@@ -0,0 +1,5 @@
+let math = Number(prompt("Tính điểm trung bình của 3 môn math, physics, chemistry \n Nhập vào điểm số môn Math:"));
+let physics = Number(prompt("Nhập vào điểm số môn Physics:"));
+let chemistry = Number(prompt("Nhập vào điểm số môn Chemistry:"));
+let tb = (math + physics + chemistry)/3;
+document.writeln("Điểm trung bình của 3 môn là: " + tb + " điểm"); 
