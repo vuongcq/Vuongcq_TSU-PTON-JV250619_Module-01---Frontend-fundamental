31f5448ae754bb185d2e41cad77fd90842fbb023
vuongcq <quyvuong991@gmail.com>
2025-07-13 15:51:49 +0900

    Add files via upload
    Upload Session10- ex5

---
+++ Session10-Ex5.js
@@ -0,0 +1,3 @@
+let usd = Number(prompt("Đây là chương trình chuyển đổi tiền tệ từ USD->VND\n Nhập vào số tiền USD cần chuyển đổi:"))
+let vnd = usd *25000;
+alert("Số tiền VNĐ là:" +" " + vnd +"đ");
