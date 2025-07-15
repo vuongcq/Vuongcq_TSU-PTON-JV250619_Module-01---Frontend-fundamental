90d298097101ee69575208f77f9d989d7382de4f
vuongcq <quyvuong991@gmail.com>
2025-07-15 23:38:54 +0900

    Add files via upload
    upload Session11-ex3

---
+++ Session11-Ex3.js
@@ -0,0 +1,36 @@
+let num = Number(prompt("Nhập vào 1 số từ 0->9:"));
+switch(num){
+    case 0:
+        alert("Số không");
+        break;
+    case 1:
+        alert("Số một");
+        break;
+    case 2:
+        alert("Số hai");
+        break;
+    case 3:
+        alert("Số ba");
+        break;
+    case 4:
+        alert("Số bốn");
+        break;
+    case 5:
+        alert("Số năm");
+        break;
+    case 6:
+        alert("Số sáu");
+        break;
+    case 7:
+        alert("Số bảy");
+        break;
+    case 8:
+        alert("Số tám");
+        break;
+    case 9:
+        alert("Số chín");
+        break;
+    default:
+        alert("Số này không nằm trong phạm vi từ 0->9");
+        break;
+}
