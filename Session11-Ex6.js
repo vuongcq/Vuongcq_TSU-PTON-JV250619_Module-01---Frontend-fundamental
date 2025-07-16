d0ebd3ef4899863dbdafb1e6f0008d812b738b54
vuongcq <quyvuong991@gmail.com>
2025-07-16 20:35:56 +0900

    Add files via upload
    update Session11-ex6

--- Session11-Ex6.js
+++ Session11-Ex6.js
@@ -3,15 +3,15 @@ let b = Number(prompt("Mời bạn nhập vào số b:"));
 let pheptinh = prompt("Mời nhập vào phép tình bạn cần tính ( + , - , x, :):")
 switch(pheptinh){
     case ("+"):
-        console.log(`Phếp cộng có kết quả là: ${a+b}`);
+        alert (`Kết quả của pháp tính trên: a+b = ${a+b}`);
         break;
     case ("-"):
-        console.log(`Phếp trừ có kết quả là: ${a-b}`);
+        alert (`PKết quả của pháp tính trên: a-b = ${a-b}`);
         break;
     case ("x"):
-        console.log(`Phếp nhân có kết quả là: ${a*b}`);
+        alert (`Kết quả của pháp tính trên: axb = ${a*b}`);
         break;
     case (":"):
-        console.log(`Phếp chia có kết quả là: ${a/b}`);
+        alert (`Kết quả của pháp tính trên: a:b = ${a/b}`);
         break;
 }
