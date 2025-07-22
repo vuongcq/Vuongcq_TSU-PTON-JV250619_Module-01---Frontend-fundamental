e4ad7f25d04815658629a81bf7da19122c03c485
vuongcq <quyvuong991@gmail.com>
2025-07-22 22:13:19 +0900

    Add files via upload
    Upload file Session14-ex1

---
+++ Session14-Ex1.js
@@ -0,0 +1,8 @@
+let person = {
+    name: "John Doe",
+    age: 25,
+    Job: "Developer",
+}
+console.log("Họ và tên: "+person.name);
+console.log("Tuổi: "+person.age);
+console.log("Công việc: "+person.Job);
